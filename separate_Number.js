f04d807601739afbb4e212ba8ba54f9c7f6eda9d
Stadionova <726383@mail.ru>
2019-09-11 08:10:21 +0300

    separate_numbers

---
+++ separate_Number.js
@@ -0,0 +1,132 @@
+// Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+// Output: 7 -> 0 -> 8
+// Explanation: 342 + 465 = 807.
+
+var firstNumArr = [2, 4, 3];
+var secondNumArr = [5, 6, 4];
+var full = [];
+
+let strFirst = firstNumArr.join('');
+let strSecond = secondNumArr.join('');
+let sum = (+strFirst) + (+strSecond);
+var strNow = String(sum);
+var separatedStr = strNow.split('');
+separatedStr.forEach(function (item, index, separatedStr) {
+    var num = Number(item);
+    full.push(num);
+});
+full.reverse();
+console.log(full);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var l1 =
+{
+    val: 2,
+    next: {
+        val: 4,
+        next: {
+            val: 3,
+            next: null,
+        }
+    }
+};
+
+
+var l2 =
+{
+    val: 5,
+    next: {
+        val: 6,
+        next: {
+            val: 4,
+            next: null,
+        }
+    }
+};
+
+var addTwoNumbers = function (firstNumLinkedList, secondNumLinkedList) {
+    var full = [];
+    let strFirst = firstNumLinkedList.join('');
+    let strSecond = secondNumLinkedList.join('');
+    let sum = (+strFirst) + (+strSecond);
+    var strNow = String(sum);
+    var separatedStr = strNow.split('');
+    separatedStr.forEach(function (item, index, separatedStr) {
+        var num = Number(item);
+        full.push(num);
+    });
+    full.reverse();
+    return full;
+};
+
+addTwoNumbers(l1, l2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var l1 =
+{
+    val: 2,
+    next: {
+        val: 4,
+        next: {
+            val: 3,
+            next: null,
+        }
+    }
+};
+
+
+var l2 =
+{
+    val: 5,
+    next: {
+        val: 6,
+        next: {
+            val: 4,
+            next: null,
+        }
+    }
+};
+
+var addTwoNumbers = function (firstNumLinkedList, secondNumLinkedList) {
+    debugger;
+    var full = [];
+    let strFirst = firstNumLinkedList.join('');
+    let strSecond = secondNumLinkedList.join('');
+    let sum = (+strFirst) + (+strSecond);
+    var strNow = String(sum);
+    var separatedStr = strNow.split('');
+    separatedStr.forEach(function (item, index, separatedStr) {
+        var num = Number(item);
+        full.push(num);
+    });
+    full.reverse();
+    return full;
+};
+
+addTwoNumbers(l1, l2);
